41b441a1fdebab1c61b950bb1ad19066edc6bdb7
Jonathan Sales <51421355+JonathanSaless@users.noreply.github.com>
2022-09-29 12:00:27 -0300

    Add files via upload

---
+++ src/ClientUDP.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.*;
+import org.jasypt.util.text.BasicTextEncryptor;
+
+class ClientUDP {
+	public static void main(String args[]) throws Exception {
+		BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPassword("senha");
+
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+		DatagramSocket clientSocket = new DatagramSocket();
+
+		int porta = Integer.parseInt(args[1]);
+
+		InetAddress IPAddress = InetAddress.getByName(args[0]);
+
+		byte[] sendData = new byte[4096];
+		byte[] receiveData = new byte[4096];
+
+		System.out.println("Digite o texto a ser enviado ao servidor: ");
+		String sentence = inFromUser.readLine();
+		sendData = sentence.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, porta);
+
+		System.out.println("Enviando pacote UDP para " + args[0] + ":" + porta);
+		clientSocket.send(sendPacket);
+
+		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+		clientSocket.receive(receivePacket);
+		System.out.println("Pacote UDP recebido...");
+
+		String modifiedSentence = new String(receivePacket.getData());
+
+		System.out.println("Texto recebido do servidor:" + modifiedSentence);
+
+		String decryptText = textEncryptor.decrypt(modifiedSentence);
+        System.out.println("TEXTO DESCRIPTOGRAFADO:" + decryptText);
+
+		clientSocket.close();
+		System.out.println("Socket cliente fechado!");
+	}
+}
---
+++ src/ServerUDP.java
@@ -0,0 +1,45 @@
+import java.net.*;
+import org.jasypt.util.text.BasicTextEncryptor;
+
+class ServerUDP {
+	public static void main(String args[]) throws Exception {
+		BasicTextEncryptor textEncryptor = new BasicTextEncryptor();     
+        textEncryptor.setPassword("senha");
+
+		int porta = Integer.parseInt(args[0]);
+		
+		try (DatagramSocket serverSocket = new DatagramSocket(porta)) {
+            byte[] receiveData;
+            byte[] sendData;
+
+            while (true) {
+            	receiveData = new byte[1024];
+				sendData = new byte[1024];
+
+            	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            	System.out.println("Esperando por datagrama UDP na porta " + porta);
+            	serverSocket.receive(receivePacket);
+            	System.out.print("Datagrama UDP recebido...");
+
+            	String sentence = new String(receivePacket.getData());
+            	System.out.println("Msg recebida do cliente " + sentence);
+            	
+            	InetAddress IPAddress = receivePacket.getAddress();
+
+            	int port = receivePacket.getPort();
+
+            	String encryptedText = textEncryptor.encrypt(sentence);
+                System.out.println("Texto Criptografado: "+encryptedText);
+
+            	sendData = encryptedText.getBytes();
+
+            	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            	
+            	System.out.print("Enviando " + sendData + "...");
+
+            	serverSocket.send(sendPacket);
+            	System.out.println("OK\n");
+            }
+        }
+	}
+}
